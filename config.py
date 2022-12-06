2ff8e634ea31c3e8376c7ba5ee283b790a50e38e
praveeendominic <61731613+praveeendominic@users.noreply.github.com>
2022-12-05 22:45:13 -0500

    remove cude from config file

--- config.py
+++ config.py
@@ -17,7 +17,8 @@ torch.manual_seed(1)
 np.random.seed(300)
 BATCH_SIZE = 32
 EPOCHS = 5
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cpu')
 TRAINING_FILE = '../Input/train/'
 TESTING_FILE = 'Input/test/'
 OUT = '../Output/'
